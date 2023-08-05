46f58bf3b3cacb092103454cbea9f10e09e13ab5
Ziek <zieksanchez3@gmail.com>
2023-08-05 08:26:05 -0700

    Edit classes on container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,7 @@
 import { ReactNode } from "react";
 
 export const Container = ({ children }: { children?: ReactNode }) => {
-  return (
-    <div className="w-full h-screen appHeight flex justify-center">
-      {children}
-    </div>
-  );
+  return <div className="w-full appHeight flex justify-center">{children}</div>;
 };
 
 export default Container;
