4219a31155db46e9b02db715210cb29eb2112416
Ziek <zieksanchez3@gmail.com>
2023-08-05 08:35:39 -0700

    Edit classes on container component

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -5,9 +5,11 @@ const PageLayout = ({ children }: { children?: ReactNode }) => {
   return (
     <div className="flex w-full h-full flex-col">
       <div className="max-w-3xl w-full mt-4 mx-6">{children}</div>
+      <p>Testing the top</p>
       <div className="mt-auto">
         <BottomNavBar />
       </div>
+      <p>Testing the bottom</p>
     </div>
   );
 };
