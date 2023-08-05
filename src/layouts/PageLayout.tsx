7e549c1fa2824c792748e55320f1bb116a89f04f
Ziek <zieksanchez3@gmail.com>
2023-08-05 08:43:30 -0700

    Remove test code and prevent zooming on mobile

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -3,13 +3,11 @@ import { BottomNavBar } from "../components/BottomNavBar";
 
 const PageLayout = ({ children }: { children?: ReactNode }) => {
   return (
-    <div className="flex w-full h-full flex-col border-2 border-cyan-300 ">
+    <div className="flex w-full h-full flex-col">
       <div className="max-w-3xl w-full mt-4 mx-6">{children}</div>
-      <p>Testing the top</p>
       <div className="mt-auto">
         <BottomNavBar />
       </div>
-      <p>Testing the bottom</p>
     </div>
   );
 };
