2025c7652c78485969fc0659e4493a7eb5be5485
Ziek <zieksanchez3@gmail.com>
2023-08-06 13:08:37 -0700

    Bug fix for importing users icon

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,13 +1,13 @@
 import { ReactComponent as HouseIcon } from "../assets/icons/house.svg";
 import { ReactComponent as CupIcon } from "../assets/icons/cup.svg";
-import { ReactComponent as Users } from "../assets/icons/Users.svg";
+import { ReactComponent as UsersIcon } from "../assets/icons/users.svg";
 
 const BottomNavBar = () => {
   return (
     <div className="h-14 flex items-center justify-between px-28 border-t-2 border-gray-50 w-full">
       <HouseIcon className="fill-purple-500" />
       <CupIcon />
-      <Users />
+      <UsersIcon />
     </div>
   );
 };
