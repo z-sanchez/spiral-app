6579bd2455e6893d78e326460c910c8094fe8c81
Ziek <zieksanchez3@gmail.com>
2023-08-06 12:57:31 -0700

    Add vite-plugin-svgr and config updates

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,5 +1,11 @@
+import { ReactComponent as Logo } from "../assets/icons/house.svg";
+
 const BottomNavBar = () => {
-  return <div className="h-14 bg-red-300">Bottom NavBar</div>;
+  return (
+    <div className="h-14 flex">
+      <Logo />
+    </div>
+  );
 };
 
 export { BottomNavBar };
