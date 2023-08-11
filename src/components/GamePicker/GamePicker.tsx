94c6acf1add5e182ba81a3f4ac935ce1f9d041e1
Ziek <zieksanchez3@gmail.com>
2023-08-11 07:23:27 -0700

    Min height adjustment

--- src/components/GamePicker/GamePicker.tsx
+++ src/components/GamePicker/GamePicker.tsx
@@ -9,7 +9,7 @@ const GamePicker = () => {
     <>
       <div className="absolute bg-gray-300 opacity-40 h-full w-full"></div>
       <div className="absolute  max-w-3xl h-full w-full flex justify-center items-center">
-        <div className="relative w-11/12 h-3/5 flex flex-col justify-between items-center bg-white rounded-xl">
+        <div className="relative w-11/12 h-3/5 min-h-[500px]  flex flex-col justify-between items-center bg-white rounded-xl">
           <div className="w-full flex items-center py-5 px-4 justify-between">
             <ExitIcon className="w-6 h-6  drop-shadow-md" />
             <p className="font-light text-gray-300 text-sm">1 of 20</p>
@@ -18,8 +18,8 @@ const GamePicker = () => {
           {/* <div className="absolute h-full w-full flex justify-center items-center">
             <LockIcon className="fill-purple-500 h-8 w-8" />
           </div> */}
-          <GameDetails />
-          {/* <TeamPicker /> */}
+          {/* <GameDetails /> */}
+          <TeamPicker />
         </div>
       </div>
     </>
