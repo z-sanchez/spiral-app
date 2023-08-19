a7ec8a9ed4103ba4ef13f08833ae2087faba6a7a
Ziek <zieksanchez3@gmail.com>
2023-08-19 07:53:57 -0700

    Updated bottom nav with routes

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,12 +1,23 @@
 import { ReactComponent as HouseIcon } from "../assets/icons/house.svg";
 import { ReactComponent as CupIcon } from "../assets/icons/cup.svg";
 import { ReactComponent as UsersIcon } from "../assets/icons/users.svg";
+import { NavLink } from "react-router-dom";
 
 const BottomNavBar = () => {
   return (
     <div className="flex-shrink-0 bg-white w-full h-14 flex items-center justify-between px-20 border-t-2 border-gray-50">
-      <HouseIcon className="fill-purple-500" />
-      <CupIcon />
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? "fill-purple-500" : "")}
+      >
+        <HouseIcon />
+      </NavLink>
+      <NavLink
+        to="/scores"
+        className={({ isActive }) => (isActive ? "fill-purple-500" : "")}
+      >
+        <CupIcon />
+      </NavLink>
       <UsersIcon />
     </div>
   );
