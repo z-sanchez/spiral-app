9603b8649d5d890f6fde1d116da9c239f6f0b405
Ziek <zieksanchez3@gmail.com>
2023-09-16 15:33:46 -0700

    Adjustments

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,6 +8,7 @@ export const FIREBASE_CONFIGURATION = {
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
   appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID,
 };
 
 export const SPIRAL_COOKIE_NAME = "spiralAppCookie";
