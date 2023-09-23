049884fd98371a3ec5029764df1adb2f1c816289
Ziek <zieksanchez3@gmail.com>
2023-09-23 11:10:39 -0700

    Remove header icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,12 @@
 import { ReactComponent as CoinIcon } from "../assets/icons/coin.svg";
-import { ReactComponent as BrightnessIcon } from "../assets/icons/brightness.svg";
 
 const Header = () => {
   return (
     <div className="flex h-16 px-6 justify-between w-full items-center flex-shrink-0">
       <div className="flex items-center">
         <CoinIcon className="stroke-purple-500" />
-        <p className="text-purple-500 font-bold text-sm ml-1">Pot $25</p>
+        <p className="text-purple-500 font-bold text-sm ml-1">Pot $250</p>
       </div>
-      <BrightnessIcon />
     </div>
   );
 };
