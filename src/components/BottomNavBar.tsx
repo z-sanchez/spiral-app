b08a52b46ae6a4b84bad2be30465a9e7afea78be
Ziek <zieksanchez3@gmail.com>
2023-09-23 15:32:24 -0700

    Nav bar size adjustment

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -23,7 +23,7 @@ const BottomNavBar = () => {
   };
 
   return (
-    <div className="flex-shrink-0 bg-white w-full h-14 flex items-center justify-between px-20 border-t-2 border-gray-50">
+    <div className="flex-shrink-0 bg-white w-full h-16 pb-4 flex items-center justify-between px-20 border-t-2 border-gray-50">
       <NavLink
         onClick={(e) => e.stopPropagation()}
         to="/"
