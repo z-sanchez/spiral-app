5af8a9038421b698b13a972b31ef4fb94df9f2ec
Ziek <zieksanchez3@gmail.com>
2023-09-23 15:43:40 -0700

    Remove unused code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="flex h-16 px-6 justify-between w-full items-center flex-shrink-0">
       <div className="flex items-center">
         <CoinIcon className="stroke-purple-500" />
-        <p className="text-purple-500 font-bold text-sm ml-1">Pot $25</p>
+        <p className="text-purple-500 font-bold text-sm ml-1">Pot $125</p>
       </div>
       <BrightnessIcon />
     </div>
