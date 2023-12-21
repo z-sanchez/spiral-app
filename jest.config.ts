b3a4c6bf26958afdce289d50438acefa10441bcc
Ziek <zieksanchez3@gmail.com>
2023-12-20 23:31:50 -0800

    Adding test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -8,8 +8,5 @@ export default {
   moduleNameMapper: {
     "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
   },
-  modulePathIgnorePatterns: [
-    "<rootDir>/src/__test__/App.test.tsx",
-    "<rootDir>/src/__test__/Game.test.tsx",
-  ],
+  modulePathIgnorePatterns: ["<rootDir>/src/__test__/App.test.tsx"],
 };
