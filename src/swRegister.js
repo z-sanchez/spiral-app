b4f56a44047a9220f6457f9d5b318b139f02601f
Ziek <zieksanchez3@gmail.com>
2024-10-26 09:12:56 -0700

    service worker update

--- src/swRegister.js
+++ src/swRegister.js
@@ -6,6 +6,13 @@ export const registerServiceWorker = () => {
         // Check for updates every 2 minutes (120000 ms)
         setInterval(() => {
           registration.update(); // Manually check for updates
+          // Listen for the service worker becoming active
+          navigator.serviceWorker.addEventListener("controllerchange", () => {
+            // If there's a new service worker controlling the page, reload it
+            if (navigator.serviceWorker.controller) {
+              window.location.reload();
+            }
+          });
           console.log("Service Worker updated register");
         }, 120000); // 2-minute interval
       })
